bb2dd8d441c824aebc03daa936df5a26fdde595d
Liz <barts.elisa@gmx.de>
2021-08-15 00:26:27 +0200

    initial commit

--- 06_main.js
+++ 06_main.js
@@ -69,8 +69,8 @@ $("document").ready(function() {
  
              // 52
              Filler_HowMany14,
-             Filler_Answer52,
-             Filler_INF52,
+             Filler_Answer50,
+             Filler_INF50,
  
              //2
              Filler_Polar2,
@@ -79,8 +79,8 @@ $("document").ready(function() {
  
              //62
              Filler_What16,
-             Filler_Answer62,
-             Filler_INF62,
+             Filler_Answer60,
+             Filler_INF60,
  
              //10
              Filler_Polar4,
@@ -89,13 +89,13 @@ $("document").ready(function() {
  
              //46
              Filler_What12,
-             Filler_Answer46,
-             Filler_INF46,
+             Filler_Answer44,
+             Filler_INF44,
  
              //43
              Filler_HowMany11,
-             Filler_Answer43,
-             Filler_INF43,
+             Filler_Answer45,
+             Filler_INF45,
  
              //12
              Filler_What4,
@@ -104,8 +104,8 @@ $("document").ready(function() {
  
              //29
              Filler_What7,
-             Filler_Answer29,
-             Filler_INF29,
+             Filler_Answer27,
+             Filler_INF27,
  
              //57
              Filler_Polar15,
@@ -158,8 +158,8 @@ $("document").ready(function() {
  
              //45
              Filler_What11,
-             Filler_Answer45,
-             Filler_INF45,
+             Filler_Answer43,
+             Filler_INF43,
  
              //7
              Filler_Did1,
@@ -178,13 +178,13 @@ $("document").ready(function() {
  
              //69
              Filler_What17,
-             Filler_Answer69,
-             Filler_INF69,
+             Filler_Answer67,
+             Filler_INF67,
  
              //68
              Filler_HowMany18,
-             Filler_Answer68,
-             Filler_INF68,
+             Filler_Answer70,
+             Filler_INF70,
  
              //65
              Filler_Polar17,
@@ -198,8 +198,8 @@ $("document").ready(function() {
  
              //36
              Filler_HowMany10,
-             Filler_Answer36,
-             Filler_INF36,
+             Filler_Answer38,
+             Filler_INF38,
  
              //48
              Filler_Did12,
@@ -251,8 +251,8 @@ $("document").ready(function() {
             
              // 30
              Filler_What8,
-             Filler_Answer30,
-             Filler_INF30,
+             Filler_Answer28,
+             Filler_INF28,
  
              //39
              Filler_Did9,
@@ -261,8 +261,8 @@ $("document").ready(function() {
  
              //19
              Filler_HowMany5,
-             Filler_Answer19,
-             Filler_INF19,
+             Filler_Answer21,
+             Filler_INF21,
  
              // 40
              Filler_Did10,
@@ -276,13 +276,13 @@ $("document").ready(function() {
  
              //61
              Filler_What15,
-             Filler_Answer61,
-             Filler_INF61,
+             Filler_Answer59,
+             Filler_INF59,
  
              //53
              Filler_What13,
-             Filler_Answer53,
-             Filler_INF53,
+             Filler_Answer51,
+             Filler_INF51,
  
              //1
              Filler_Polar1,
@@ -291,8 +291,8 @@ $("document").ready(function() {
  
              //35
              Filler_HowMany9,
-             Filler_Answer35,
-             Filler_INF35,
+             Filler_Answer37,
+             Filler_INF37,
  
              //15
              Filler_Did3,
@@ -301,13 +301,13 @@ $("document").ready(function() {
  
              //22
              Filler_What6,
-             Filler_Answer22,
-             Filler_INF22,
+             Filler_Answer20,
+             Filler_INF20,
  
              //38
              Filler_What10,
-             Filler_Answer38,
-             Filler_INF38,
+             Filler_Answer36,
+             Filler_INF36,
 
 
              // COND 1 (polar comp)
@@ -347,8 +347,8 @@ $("document").ready(function() {
             // FILLERS??
              // 28
              Filler_HowMany8,
-             Filler_Answer28,
-             Filler_INF28,
+             Filler_Answer30,
+             Filler_INF30,
  
              //56
              Filler_Did14,
@@ -372,8 +372,8 @@ $("document").ready(function() {
  
              //37
              Filler_What9,
-             Filler_Answer37,
-             Filler_INF37,
+             Filler_Answer35,
+             Filler_INF35,
  
              //26
              Filler_Polar8,
@@ -402,8 +402,8 @@ $("document").ready(function() {
  
              //70
              Filler_What18,
-             Filler_Answer70,
-             Filler_INF70,
+             Filler_Answer68,
+             Filler_INF68,
 
 
 
@@ -442,8 +442,8 @@ $("document").ready(function() {
             // FILLERS??
             // 51
             Filler_HowMany13,
-            Filler_Answer51,
-            Filler_INF51,
+            Filler_Answer53,
+            Filler_INF53,
 
             //25
             Filler_Polar7,
@@ -452,8 +452,8 @@ $("document").ready(function() {
 
             //27
             Filler_HowMany7,
-            Filler_Answer27,
-            Filler_INF27,
+            Filler_Answer29,
+            Filler_INF29,
 
             // 11
             Filler_What3,
@@ -477,13 +477,13 @@ $("document").ready(function() {
 
             //67
             Filler_HowMany17,
-            Filler_Answer67,
-            Filler_INF67,
+            Filler_Answer69,
+            Filler_INF69,
 
             //60
             Filler_HowMany16,
-            Filler_Answer60,
-            Filler_INF60,
+            Filler_Answer62,
+            Filler_INF62,
 
             //49
             Filler_Polar13,
@@ -497,8 +497,8 @@ $("document").ready(function() {
 
             //59
             Filler_HowMany15,
-            Filler_Answer59,
-            Filler_INF59,
+            Filler_Answer61,
+            Filler_INF61,
     
 
 
@@ -539,13 +539,13 @@ $("document").ready(function() {
             // FILLER??
             // 54
             Filler_What14,
-            Filler_Answer54,
-            Filler_INF54,
+            Filler_Answer52,
+            Filler_INF52,
 
             //20
             Filler_HowMany6,
-            Filler_Answer20,
-            Filler_INF20,
+            Filler_Answer22,
+            Filler_INF22,
 
             //17
             Filler_Polar5,
@@ -569,13 +569,13 @@ $("document").ready(function() {
 
             //21
             Filler_What5,
-            Filler_Answer21,
-            Filler_INF21,
+            Filler_Answer19,
+            Filler_INF19,
 
             //44
             Filler_HowMany12,
-            Filler_Answer44,
-            Filler_INF44,
+            Filler_Answer46,
+            Filler_INF46,
 
             //50
             Filler_Polar14,
